5d770bbfb63d3c0bc359fa1691c407693fe5c92e
marshallhhhh <marshall.hodges.00@gmail.com>
2025-09-07 20:54:39 +1000

    compiled to WASM

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,15 @@ pub mod permute;
 pub mod keygen;
 pub mod sboxes;
 pub mod sdes;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn encrypt(plaintext: u16, key: u16) -> u16 {
+    sdes::encrypt(plaintext, key)
+}
+
+#[wasm_bindgen]
+pub fn decrypt(ciphertext: u16, key: u16) -> u16 {
+    sdes::decrypt(ciphertext, key)
+}
